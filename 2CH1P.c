e18dc1ea7302a0b513a6af1468d2640fcb3419db
HillsKalathiya <100057465+manishkalathiya@users.noreply.github.com>
2024-01-20 17:38:03 +0530

    Add files via upload

---
+++ 2CH1P.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+	printf("12 + 6 = 18");
+	printf("12 - 6 = 6");
+	printf("12 * 6 = 72");
+	printf("12 / 6 = 2");
+	printf("12 % 6 = 0"):
+	getch();
+}
